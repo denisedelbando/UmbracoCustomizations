c9d605d1979c28cd3ccc4c39cca23c8cdac9d19a
Denise Michelle del Bando <avatart0ph@users.noreply.github.com>
2017-06-28 16:34:49 -0400

    - create readonly data type - fix some areas for custom propertypicker

---
+++ DT.UmbracoCustomizations/App_Plugins/DTCustomReadonly/js/CustomReadOnly.PreValue.controller.js
@@ -0,0 +1,46 @@
+﻿function CustomReadOnlyPrevalueController($scope, $element, assetsService, dialogService, angularHelper, $timeout) {
+
+    //tell the assets service to load the markdown.editor libs from the markdown editors
+    //plugin folder
+
+    if ($scope.model.value === null || $scope.model.value === "") {
+        $scope.model.value = "### https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet";
+    }
+    
+    assetsService
+        .load([
+            "lib/markdown/markdown.converter.js",
+            "lib/markdown/markdown.sanitizer.js",
+            "lib/markdown/markdown.editor.js"
+        ])
+        .then(function () {
+
+            // we need a short delay to wait for the textbox to appear.
+            setTimeout(function () {
+                //this function will execute when all dependencies have loaded
+                // but in the case that they've been previously loaded, we can only
+                // init the md editor after this digest because the DOM needs to be ready first
+                // so run the init on a timeout
+                $timeout(function () {
+                    var converter2 = new Markdown.Converter();
+                    var editor2 = new Markdown.Editor(converter2, "-" + $scope.model.alias);
+                    editor2.run();
+                    
+                    editor2.hooks.set("onPreviewRefresh", function () {
+                        // We must manually update the model as there is no way to hook into the markdown editor events without exstensive edits to the library.
+                        if ($scope.model.value !== $("textarea", $element).val()) {
+                            angularHelper.getCurrentForm($scope).$setDirty();
+                            $scope.model.value = $("textarea", $element).val();
+                        }
+                    });
+
+                }, 200);
+            });
+
+            //load the seperat css for the editor to avoid it blocking our js loading TEMP HACK
+            assetsService.loadCss("lib/markdown/markdown.css");
+        })
+}
+
+
+angular.module("umbraco").controller("Custom.ReadOnlyPrevalueController", CustomReadOnlyPrevalueController);
---
+++ DT.UmbracoCustomizations/App_Plugins/DTCustomReadonly/js/CustomReadOnly.controller.js
@@ -0,0 +1,26 @@
+﻿function CustomReadOnlyController ($scope, assetsService, $timeout) {
+    $scope.htmlPreValue = $scope.model.config.prevalueText;
+    assetsService
+    .load([
+        "lib/markdown/markdown.converter.js"
+    ])
+    .then(function () {
+
+        // we need a short delay to wait for the textbox to appear.
+        setTimeout(function () {
+            //this function will execute when all dependencies have loaded
+            // but in the case that they've been previously loaded, we can only
+            // init the md editor after this digest because the DOM needs to be ready first
+            // so run the init on a timeout
+            $timeout(function () {
+                var converter2 = new Markdown.Converter();
+                $scope.htmlPreValue = converter2.makeHtml($scope.model.config.prevalueText);
+            }, 200);
+        });
+
+        //load the seperat css for the editor to avoid it blocking our js loading TEMP HACK
+        assetsService.loadCss("lib/markdown/markdown.css");
+    })
+}
+
+angular.module("umbraco").controller("Custom.ReadOnlyController", CustomReadOnlyController);
